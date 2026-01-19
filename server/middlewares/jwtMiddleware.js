2849eea54faaa614a0862beb06e243684a8a0e65
Harsh <kumarharsh93982@gmail.com>
2026-01-20 00:48:20 +0530

    Removed log for token expire or invalid.

--- server/middlewares/jwtMiddleware.js
+++ server/middlewares/jwtMiddleware.js
@@ -26,7 +26,6 @@ export const isAuthenticated = (req, res, next) => {
         next();
 
     } catch (error) {
-        console.log(error);
         return res.status(401).json({
             sucess: false,
             message: 'Token is invalid or expired'
