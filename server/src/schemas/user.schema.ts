a14dadd4b9fb9b6fc4b65c818bc05746e0ce677a
Harsh <kumarharsh93982@gmail.com>
2026-03-06 16:45:27 +0530

    Validation of schema using zod.

---
+++ server/src/schemas/user.schema.ts
@@ -0,0 +1,12 @@
+// This is for validation of the data by the user/client
+
+import { z } from "zod";
+
+const userSchema = z.object({
+  email: z.email(),
+  password: z.string().min(6),
+  name: z.string().min(3),
+  phoneNumber: z.string().min(10),
+});
+
+export default userSchema;
