ffdf2e99e86b67d9e6dc9bbe52b6724a5cae360e
Harsh <kumarharsh93982@gmail.com>
2026-03-08 00:32:11 +0530

    Signup and login schema update.

--- server/src/schemas/signup.schema.ts
+++ server/src/schemas/signup.schema.ts
@@ -2,11 +2,11 @@
 
 import { z } from "zod";
 
-const userSchema = z.object({
+const signupSchema = z.object({
   email: z.email(),
   password: z.string().min(6),
   name: z.string().min(3),
   phoneNumber: z.string().min(10),
 });
 
-export default userSchema;
+export default signupSchema;
