b0ea421340ed2285727dd5830b487910b5af5e29
Harsh <kumarharsh93982@gmail.com>
2026-03-08 13:08:07 +0530

    helmet and cookie-parser as a global middleware.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express, { Application } from "express";
 import cors from "cors";
+import helmetConfig from "./config/helmet.config.js"
+import cookieParser from "cookie-parser";
 
 // Configurations & Routes
 import { corsOptions } from "./config/cors.config.js";
@@ -11,10 +13,12 @@ const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 // 1. Global Middleware
+app.use(helmetConfig);
 app.use(cors(corsOptions));
-app.use(responseTime);
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(responseTime);
 
 // 2. Health Check (Keep it simple)
 app.get("/health", (req, res) => {
