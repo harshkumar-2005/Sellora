1a494386472c91139b28b435b78f3a46842eb013
Harsh <kumarharsh93982@gmail.com>
2026-03-08 17:53:50 +0530

    custom req: AuthRequest for jwt based routes.

--- server/src/types/express.types.ts
+++ server/src/types/express.types.ts
@@ -1,6 +1,8 @@
 import { Request } from "express";
 import { JWTPayload } from "./jwt.types.js";
 
+// Use req: AuthRequest for routes/middleware that require access to the authenticated user (e.g., after JWT verification).
 export interface AuthRequest extends Request {
   user?: JWTPayload;
 }
+// Use req: Request for routes/middleware that don’t need user info.
