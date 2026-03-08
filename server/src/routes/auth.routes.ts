b9f5f4f00d1c08903361932f0af5a1ea0701bdff
Harsh <kumarharsh93982@gmail.com>
2026-03-08 18:04:04 +0530

    C:/Program Files/Git/refresh and /logout routes are added.

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, login, getme } from "../controllers/auth.controller.js";
+import { signup, login, getme, refresh, logout } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -11,5 +11,9 @@ router.post('/signup', signup);
 router.post('/login', login);
 
 router.post('/getme', protectRoute, getme);
+
+router.post('/refresh', protectRoute, refresh);
+
+router.post('/logout', protectRoute, logout);
 
 export default router;
