57bba44adc2615dffebf1f42c539c6b8b4114f27
Harsh <kumarharsh93982@gmail.com>
2026-03-08 13:04:57 +0530

    C:/Program Files/Git/signup, /login, /getme this routes are done.

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,16 +1,15 @@
 import express from "express";
-
 import { signup, login, getme } from "../controllers/auth.controller.js";
-
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 // /v1/api/auth
 
-router.get('/signup', signup);
+router.post('/signup', signup);
 
-router.get('/login', login);
+router.post('/login', login);
 
-router.get('/getme', getme);
+router.post('/getme', protectRoute, getme);
 
 export default router;
