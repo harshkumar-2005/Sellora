461ce7b6fddaa9123a7dfbf42206fdfd1b926904
Harsh <kumarharsh93982@gmail.com>
2026-03-09 21:26:56 +0530

    master index updated with cart route.

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js";
 import productRoutes from "./product.routes.js";
+import cartRoutes from "./cart.routes.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 
 router.use("/products", productRoutes);
+
+router.use("/cart", cartRoutes);
 
 export default router;
