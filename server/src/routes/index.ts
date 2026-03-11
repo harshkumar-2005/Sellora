b1f20c6f48ef9a9fd0c5012af62be8bf3d65b2bb
Harsh <kumarharsh93982@gmail.com>
2026-03-12 00:36:30 +0530

    review endpoint (review-of-product).

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,6 +4,7 @@ import productRoutes from "./product.routes.js";
 import cartRoutes from "./cart.routes.js";
 import orderRoutes from "./order.routes.js";
 import wishlistRoutes from "./wishlist.routes.js";
+import reviewRoutes from "./review.routes.js"
 
 const router = Router();
 
@@ -16,5 +17,7 @@ router.use("/cart", cartRoutes);
 router.use("/orders", orderRoutes);
 
 router.use("/wishlist", wishlistRoutes);
+
+router.use("/products", reviewRoutes);
 
 export default router;
